2663cc8577b49134b93b9e7cd9dec0430409e4ef
Jose Galindo <jgalindo@cs.com.gt>
2018-09-04 11:37:04 -0600

    Cambios

---
+++ config/csgtcrud.php
@@ -0,0 +1,8 @@
+<?php
+return [
+	'datatables' => [
+		'js'  => '/js/datatables.min.js',
+		'css' => '/css/datatables.min.css',
+	],
+	'font-awesome' => '/css/font-awesome.min.css'
+];
---
+++ config/csgtlayout.php
@@ -0,0 +1,20 @@
+<?php
+return [
+	/*
+	|--------------------------------------------------------------------------
+	| Skin
+	|--------------------------------------------------------------------------
+	|
+	| Skin LTE a utilizar por default.  skin-XXXXX
+	|
+	*/
+
+	'adminlte-skin' => 'skin-blue',
+
+	'show' => [
+			'right-sidebar'    => false,
+			'notifications'    => false,
+			'usuario-acciones' => false,
+	 ],
+	 
+];
