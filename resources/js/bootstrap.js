daba54efcb217e36ade9474e4e251ea36560fb67
menene <efmarroquinr@gmail.com>
2022-09-08 12:35:25 -0600

    vite config

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,16 +1,40 @@
 import _ from 'lodash';
 window._ = _;
 
+import $ from "jquery"; 
+window.$ = window.jQuery = $;
+
+/**
+ * We'll load jQuery and the Bootstrap jQuery plugin which provides support
+ * for JavaScript based Bootstrap features such as modals and tabs. This
+ * code may be modified to fit the specific needs of your application.
+ */
+
+// try {
+//     window.Popper = require('popper.js').default;
+    // window.$ = window.jQuery = import('jquery');
+//     window.toastr = require('toastr')
+//     require('bootstrap');
+// } catch (e) {}
+
+import Moment from "moment";
+window.moment = Moment;
+
+import 'bootstrap';
+import 'admin-lte';
+
+import DataTable from "datatables.net-bs4";
+DataTable(window, window.$);
+
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-import axios from 'axios';
-window.axios = axios;
+// window.axios = require('axios');
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+// window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
@@ -20,15 +44,18 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 // import Echo from 'laravel-echo';
 
-// import Pusher from 'pusher-js';
-// window.Pusher = Pusher;
+// window.Pusher = require('pusher-js');
 
 // window.Echo = new Echo({
 //     broadcaster: 'pusher',
-//     key: import.meta.env.VITE_PUSHER_APP_KEY,
-//     wsHost: import.meta.env.VITE_PUSHER_HOST ?? `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
-//     wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
-//     wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
-//     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
-//     enabledTransports: ['ws', 'wss'],
+//     key: process.env.MIX_PUSHER_APP_KEY,
+//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+//     forceTLS: true
 // });
+
+import axios from 'axios';
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+axios.defaults.withCredentials = true;
+window.axios = axios;
+
+export { axios };
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,34 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue2'
+import inject from '@rollup/plugin-inject';
 
 export default defineConfig({
     plugins: [
+        inject({
+            $: 'jquery',
+        }),
+        vue(),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/sass/app.scss', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    server: {
+        hmr: {
+            host: 'localhost'
+        }
+    },
+    resolve: {
+        alias: [
+            {
+                find: /^~(.*)$/,
+                replacement: '$1',
+            },
+            {
+                find: 'vue',
+                replacement: 'vue/dist/vue.js',
+            },
+        ],
+    },
 });
