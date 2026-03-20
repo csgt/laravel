df8d8d440f943edce242e27728cdd765a49d9b87
Jose Galindo <jgalindo@softlogic.com.gt>
2026-03-20 12:48:55 -0600

    fix: fontawesome static copy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
     plugins: [
@@ -13,7 +14,14 @@ export default defineConfig({
             ],
             refresh: true,
         }),
-        tailwindcss(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: "node_modules/@fortawesome/fontawesome-free/webfonts",
+                    dest: "resources",
+                },
+            ],
+        }),
     ],
     server: {
         hmr: {
